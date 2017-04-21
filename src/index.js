328c35de6736d32aaf4bf27af9a7b551bd64f85a
Ed Ropple <ed@edropple.com>
2017-04-20 23:28:33 -0400

    v0.2.0 - updated to new module API

--- src/index.js
+++ src/index.js
@@ -1,63 +1,63 @@
-import _ from 'lodash';
+import Module from '@bossmodecg/module';
 
-import BossmodeCG from '@bossmodecg/core';
+const DEFAULT_CONFIG = {};
 
-const DEFAULT_CONFIG = {
-  countInterval: 1000
-};
-
-export default class ClienteleModule extends BossmodeCG.BModule {
+export default class ClienteleModule extends Module {
   constructor(config) {
-    super("clientele", _.merge({}, DEFAULT_CONFIG, config),
+    super("clientele", config,
           { shouldCacheState: false, internalStateUpdatesOnly: true });
 
     this._state = { unauthenticatedClients: {}, authenticatedClients: {} };
+
+    this.on("internal.registerServer", (server) => {
+      server.on('internal.clientConnected', (client) => {
+        const change = {
+          unauthenticatedClients: {}
+        };
+
+        change.unauthenticatedClients[client.id] = {
+          id: client.id,
+          address: client.request.connection.remoteAddress
+        };
+
+        this.setState(change);
+        this.emit("clientConnected", change.unauthenticatedClients[client.id]);
+      });
+
+      server.on('internal.clientAuthenticated', (client) => {
+        const change = {
+          unauthenticatedClients: {},
+          authenticatedClients: {}
+        };
+
+        change.unauthenticatedClients[client.id] = null;
+        change.authenticatedClients[client.id] = {
+          id: client.id,
+          address: client.request.connection.remoteAddress,
+          identifier: client.identity.identifier,
+          clientType: client.identity.clientType
+        };
+
+        this.setState(change);
+        this.emit("clientAuthenticated", change.authenticatedClients[client.id]);
+      });
+
+      server.on('internal.clientDisconnected', (client) => {
+        const change = {
+          unauthenticatedClients: {},
+          authenticatedClients: {}
+        };
+
+        change.unauthenticatedClients[client.id] = null;
+        change.authenticatedClients[client.id] = null;
+
+        this.setState(change);
+        this.emit("clientDisconnected", { id: client.id });
+      });
+    });
   }
 
-  async _doRegister() {
-    this.server.on('internal.clientConnected', (client) => {
-      const change = {
-        unauthenticatedClients: {}
-      };
-
-      change.unauthenticatedClients[client.id] = {
-        id: client.id,
-        address: client.request.connection.remoteAddress
-      };
-
-      this.setState(change);
-      this.emit("clientConnected", change.unauthenticatedClients[client.id]);
-    });
-
-    this.server.on('internal.clientAuthenticated', (client) => {
-      const change = {
-        unauthenticatedClients: {},
-        authenticatedClients: {}
-      };
-
-      change.unauthenticatedClients[client.id] = null;
-      change.authenticatedClients[client.id] = {
-        id: client.id,
-        address: client.request.connection.remoteAddress,
-        identifier: client.identity.identifier,
-        clientType: client.identity.clientType
-      };
-
-      this.setState(change);
-      this.emit("clientAuthenticated", change.authenticatedClients[client.id]);
-    });
-
-    this.server.on('internal.clientDisconnected', (client) => {
-      const change = {
-        unauthenticatedClients: {},
-        authenticatedClients: {}
-      };
-
-      change.unauthenticatedClients[client.id] = null;
-      change.authenticatedClients[client.id] = null;
-
-      this.setState(change);
-      this.emit("clientDisconnected", { id: client.id });
-    });
-  }
+  /* eslint-disable class-methods-use-this */
+  get defaultConfig() { return DEFAULT_CONFIG; }
+  /* eslint-enable class-methods-use-this */
 }
